9c72d1dbf050eeea7cc4631688a82aedd52ff05d
Sunny299317 <gun.lovely2558@gmail.com>
2026-06-29 12:04:39 +0700

    first commit

---
+++ lab2_exercise3.dart
@@ -0,0 +1,21 @@
+void main(){
+  List<String> subjects = ['OOP','Mobile App',' Computer Network'];
+
+  print('Number of subjects :  ${subjects.length}' );
+  print('First subject :  ${subjects[0]}');
+  print('Last subjects : ${subjects[subjects.length-1]}');
+
+  subjects.add('English');
+  print('Updated subjects: $subjects');
+
+  print('-----');
+
+  Map<String,int> StudentScores = {
+    'OOP': 78,
+    'Mobile App': 80 
+  };
+ print('Scores for OOP: ${StudentScores['OOP']}');
+ StudentScores['Computer Network'] = 82;
+ print('Updated scores: $StudentScores');
+
+}
