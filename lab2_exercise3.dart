8a8a698d7aa75c7663d40a82d0712b794776d137
Sunny299317 <gun.lovely2558@gmail.com>
2026-06-30 22:07:50 +0700

    เสร็จแล้ว

--- lab2_exercise3.dart
+++ lab2_exercise3.dart
@@ -17,10 +17,17 @@ void main(){
  print('Scores for OOP: ${StudentScores['OOP']}');
  StudentScores['Computer Network'] = 82;
  print('Updated scores: $StudentScores');
+  print('Subjects containing "a"');
+  StudentScores.forEach((subject, scores) {
+    if (subject.contains('a') || subject.contains('A')) {
+      print('$subject: $scores');
+      
+      }
+});
+  StudentScores.forEach((subject, scores){
+    if (scores > 50) {
+      print('$subject: $scores');
+    }
+  });  
 
-
-print('-----');
-print('-----');
-print('-----');
-
-}
+  }
